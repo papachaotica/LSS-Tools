6af81c319a3c73e7c3bfdab18306e7707ec883fe
Marcel Hüttenberger <papachaotica@users.noreply.github.com>
2026-07-24 00:13:40 +0200

    Change download and update URLs in user script
    Updated download and update URLs to use raw.github.com.

--- dist/FirstResponder.user.js
+++ dist/FirstResponder.user.js
@@ -11,8 +11,8 @@
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
 // @connect      api.lss-manager.de
-// @downloadURL  https://github.com/papachaotica/LSS-Tools/dist/raw/refs/heads/main/FirstResponder.user.js
-// @updateURL    https://github.com/papachaotica/LSS-Tools/dist/raw/refs/heads/main/FirstResponder.user.js
+// @downloadURL  http://raw.github.com/papachaotica/LSS-Tools/main/dist/FirstResponder.user.js
+// @updateURL    http://raw.github.com/papachaotica/LSS-Tools/main/dist/FirstResponder.user.js
 // ==/UserScript==
 
 /*
