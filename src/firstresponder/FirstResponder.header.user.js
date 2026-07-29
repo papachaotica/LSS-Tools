438d715f9ea09cb69092de26c15045df1509b397
Marcel Hüttenberger <papachaotica@users.noreply.github.com>
2026-07-29 14:38:40 +0200

    renamed:    Dev/Header. txt -> dev-tools/Header.txt new file:   src/firstresponder/FirstResponder.bos-ernie.js new file:   src/firstresponder/FirstResponder.header.user.js new file:   src/firstresponder/FirstResponder.ui.js

---
+++ src/firstresponder/FirstResponder.header.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        [LSS] Ersthelfer
+// @namespace   papachaotica.leitstellenspiel.de
+// @version     2.0.0-rc2
+// @license     AGPL-3.0-or-later
+// @author      BOS-Ernie (Orginal) / papachaotica (Erweiterung)
+// @description Wählt das nächstliegende Ersthelferfahrzeug aus; Fahrzeuge können über ein Menü konfiguriert werden.
+// @run-at      document-idle
+// @match       https://*.leitstellenspiel.de/*
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_xmlhttpRequest
+// @connect     api.lss-manager.de
+// @downloadURL http://raw.github.com/papachaotica/LSS-Tools/main/dist/FirstResponder.user.js
+// @updateURL   http://raw.github.com/papachaotica/LSS-Tools/main/dist/FirstResponder.user.js
+// @require	http://localhost:8080/src/firstresponder/FirstResponder.ui.js
+// @require	http://localhost:8080/src/firstresponder/FirstResponder.bos-ernie.js
+// ==/UserScript==
+
+/*
+ * Based on original work by BOS-Ernie.
+ *
+ * Original license:
+ * BSD-3-Clause
+ *
+ * Modified and extended by papachaotica.
+ * Current project license:
+ * AGPL-3.0-or-later
+ */
+(funktion ()) {
+}();
